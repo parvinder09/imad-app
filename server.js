f5d29da81fb8932af9959fa3f77b91d576926351
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-03 02:13:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/article-one',function(req,res){
     res.send("Article one served");
 });
+
+app.get('/article-two',function(req,res){
+    res.send("Article second served");
+});
+
+app.get('/article-three',function(req,res){
+    res.send("Article third served");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
