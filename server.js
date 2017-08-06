28778fdcf8fdd2dee7d0afcdc7ad06e4f35d136c
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-06 16:18:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:"Article One | Parvinder",
+    heading:"Article One",
+    date:"6 Aug 2017",
+    content:`<p>This is article one. This is article one. This is article one.
+            This is article one. This is article one. This is article one.
+            This is article one. This is article one. This is article one.
+            This is article one. This is article one. This is article one.
+            This is article one. This is article one. This is article one.
+            This is article one. This is article one. This is article one.</p>`
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
