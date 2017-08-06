572bea2d9cca6dc3499d715ca5a65cf093397dac
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-06 17:32:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articelName',function(req,res){
-    
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
