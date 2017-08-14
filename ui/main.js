d9db3e8ef1805d8b2b92ac8cc8b423b50b624a15
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-14 18:30:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,25 @@
 console.log('Loaded!');
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
     
+    var request= new XMLHttpRequest();
     
-    counter=counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+    //create new request object
+    request.onreadystatchange=function(){
+        
+        if(request.readyState===XMLHttpRequest.DONE){
+            
+            if(request.status===200){
+                var counter=request.responseText();
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+            }
+        }
+    }
+    
+   
+   //make request
+   request.open("GET","http://parvindersharma09.imad.hasura-app.io/counter",true);
+   request.send(null);
 }
