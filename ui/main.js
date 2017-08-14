87937e47ad0fe363225d140b7222f6749bb4bd15
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-14 18:06:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var button=document.getElementById("counter");
+var counter=0;
+button.onClick=function(){
+    
+    
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter;
+}
