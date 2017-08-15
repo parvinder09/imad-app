4b11ec27e918a6ce23838c5478c088d5cc88c984
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-15 16:17:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,14 @@ app.get('/:articleName',function(req,res){
     res.send(createTemplate(articles[articleName]));
 });
 
-
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+    
+    var name=req.params.name;
+    
+    names.push(name);
+    res.send(JSON.stringify(names));
+})
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
