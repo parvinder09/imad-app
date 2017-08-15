3fd5fdd8b8c95c56205f74412ad85308dfee8a12
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-15 16:26:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,11 +31,31 @@ var submit=document.getElementById('btn_submit');
 submit.onclick=function(){
     //make a request to server
     
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
+    var request= new XMLHttpRequest();
+   
+    //create new request object
+    request.onreadystatechange = function(){
+       
+        if(request.readyState===XMLHttpRequest.DONE){
+          
+            if(request.status===200){
+               var names=request.responseText;
+               names=JSON.parse(names);
+               var list='';
+                for(var i=0;i<names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+                }
+                
+                var ul=document.getElementById('name_list');
+                ul.innerHTML=list;
+            }
+        }
     }
-    var ul=document.getElementById('name_list');
-    ul.innerHTML=list;
+    
+   
+   //make request
+   request.open("GET","http://parvindersharma09.imad.hasura-app.io/submit-name?name="+name,true);
+   request.send();
+    
+    
 }
