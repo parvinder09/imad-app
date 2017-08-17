fc669aa07f2516df22ff74a3df41b885dc5a8ac0
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-17 20:53:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
 
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
-    pool.query('SELECT * FROM user',function(err,result){
+    pool.query('SELECT * FROM article',function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
