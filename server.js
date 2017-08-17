c85f655fcbf172073a02693823b3c2c0fb4c6e58
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-17 20:31:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     user:'parvindersharma09',
     database:'parvindersharma09',
     host:'http://db.imad.hasura-app.io',
-    port:'5432',
+    port:'',
     password:process.env.DB_PASSWORD
 }
 var app = express();
