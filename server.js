1f476c5b3715fdbb12f17ae09ed41e46b1089f52
Parvinder Kumar <parvindersharma09@gmail.com>
2017-08-19 20:10:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/login',function(req,res){
         }
         else{
             if(result.rows.length===0){
-                res.status(403).send('Username/Password is invalid');
+                res.send(403).send('Username/Password is invalid');
             }
             else{
                 var dbString=result.rows[0].password;
